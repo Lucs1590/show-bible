ad6ba0e9e3bacf3ddd20da977c1c4ec35f306c25
Lucas Brito <lucasbsilva29@gmail.com>
2020-04-07 23:15:17 -0300

    karma config

--- my.conf.js
+++ my.conf.js
@@ -63,6 +63,8 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    failOnEmptyTestSuite: false 
   })
 }
